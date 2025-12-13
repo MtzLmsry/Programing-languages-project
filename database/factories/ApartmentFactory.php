7707e1a6307c9128e41ba9824f7072af3d822697
MtzLmsry <haitm_1969@icloud.com>
2025-12-13 08:54:22 -0700

    fixing the logic of factories and seeders

--- database/factories/ApartmentFactory.php
+++ database/factories/ApartmentFactory.php
@@ -6,40 +6,52 @@ use App\Models\Apartment;
 use App\Models\User;
 use App\Models\City;
 use App\Models\Governorate;
-use App\Models\Apartment_photo;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ApartmentFactory extends Factory
 {
     protected $model = Apartment::class;
 
-    public function definition()
+    public function definition(): array
     {
+        $type = $this->faker->randomElement(['one_room', 'multipul_rooms']);
+
+        if ($type === 'one_room') {
+            $rooms = 1;
+            $area  = $this->faker->numberBetween(30, 80);
+        } else {
+            $rooms = $this->faker->numberBetween(2, 5);
+            $area  = $this->faker->numberBetween(90, 200);
+        }
+
         return [
-            'owner_id' => User::inRandomOrder()->first()->id ?? User::factory(),
+            'owner_id' => User::inRandomOrder()->first()->id,
             'city_id' => City::inRandomOrder()->first()->id,
             'governorate_id' => Governorate::inRandomOrder()->first()->id,
-            'title' => $this->faker->sentence(),
-            'price' => $this->faker->numberBetween(100, 700),
-            'rooms' => $this->faker->numberBetween(1,5),
-            'floor_number' => $this->faker->numberBetween(1,10),
-            'area' => $this->faker->numberBetween(40,200),
-            'apartment_type' => $this->faker->randomElement(['one_room','multipul_rooms']),
+            'title' => $this->faker->sentence(3),
+            'price' => $this->faker->numberBetween(100, 1000),
+            'rooms' => $rooms,
+            'floor_number' => $this->faker->numberBetween(0, 5),
+            'area' => $area,
+            'apartment_type' => $type,
             'description' => $this->faker->paragraph(),
             'is_internet_available' => $this->faker->boolean(),
             'is_air_conditioned' => $this->faker->boolean(),
             'is_cleaning_available' => $this->faker->boolean(),
-            'is_electricity_available' => $this->faker->boolean(),
+            'is_electricity_available' => true,
             'is_furnished' => $this->faker->boolean(),
             'status' => 'approved',
         ];
     }
+
     public function configure()
     {
         return $this->afterCreating(function (Apartment $apartment) {
-            Apartment_photo::factory(4)->create([
-                'apartment_id' => $apartment->id
-            ]);
+            \App\Models\Apartment_photo::factory()
+                ->count(rand(4, 6))         
+                ->create([
+                    'apartment_id' => $apartment->id
+                ]);
         });
     }
 }
